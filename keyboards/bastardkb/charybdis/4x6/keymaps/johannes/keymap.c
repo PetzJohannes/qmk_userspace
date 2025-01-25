69df4d1370260379b38a1e0a959a06ee32ded7d0
Johannes Petz <johannes.petz@redmedical.de>
2025-01-25 20:41:52 +0100

    feat: switch back backspace and space

--- keyboards/bastardkb/charybdis/4x6/keymaps/johannes/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/johannes/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LCTL,    PT_Y,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, DE_MINUS, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                   TO_SPECIAL_KEYS, TO_NAVIGATION,  KC_LGUI,     TO_POINTER,  ENTER_RALT_ON_HOLD,
-                                            KC_BSPC, KC_SPC,     KC_DEL
+                                           KC_SPC, KC_BSPC,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
