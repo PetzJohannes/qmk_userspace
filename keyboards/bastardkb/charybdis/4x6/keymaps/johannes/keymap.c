db096e81abf046906f47ec8d77c60f3c0398161a
PetzJohannes <mail@johannespetz.de>
2025-02-12 07:36:44 +0100

    switch backspace and space

--- keyboards/bastardkb/charybdis/4x6/keymaps/johannes/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/johannes/keymap.c
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LCTL,    PT_Y,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, DE_MINUS, KC_RSFT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_SPC, KC_BSPC,  KC_LGUI,     TO_POINTER,  ENTER_RALT_ON_HOLD,
+                                  KC_BSPC, KC_SPC,  KC_LGUI,     TO_POINTER,  ENTER_RALT_ON_HOLD,
                 TO_NUMERIC_KEYS_LALT_ON_HOLD, TO_NAVIGATION,     KC_DEL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
